8b8cae9415af4a7fc229e75355cf62fff8771b39
artginzburg <art.ginzburg@ya.ru>
2021-10-22 01:18:28 +0300

    try different approaches

---
+++ bitrix-response.js
@@ -0,0 +1,12 @@
+const example = {
+  event: 'ONCRMDEALADD',
+  data: { FIELDS: { ID: '277' } },
+  ts: '1634848738',
+  auth: {
+    domain: 'b24-sp9gpy.bitrix24.ru',
+    client_endpoint: 'https://b24-sp9gpy.bitrix24.ru/rest/',
+    server_endpoint: 'https://oauth.bitrix.info/rest/',
+    member_id: 'f48783b2455adff9acdc00f3e6b2d78f',
+    application_token: '8ng5pm4ym2r3hy4rapplypfhx7r99ze9',
+  },
+};
--- index.js
+++ index.js
@@ -1,28 +1,63 @@
 const express = require('express');
 const axios = require('axios');
 
+const { PORT = 3000 } = process.env;
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/:url', async (req, res) => {
+axios({
+  method: 'get',
+  url: 'https://b24-sp9gpy.bitrix24.ru/rest/crm.deal.get?auth=8ng5pm4ym2r3hy4rapplypfhx7r99ze9&ID=275',
+  // auth: {
+  //   domain: 'b24-sp9gpy.bitrix24.ru',
+  //   client_endpoint: 'https://b24-sp9gpy.bitrix24.ru/rest/',
+  //   server_endpoint: 'https://oauth.bitrix.info/rest/',
+  //   member_id: 'f48783b2455adff9acdc00f3e6b2d78f',
+  //   application_token: '8ng5pm4ym2r3hy4rapplypfhx7r99ze9',
+  // },
+  data: {
+    // id: 275,
+    // auth: {
+    //   // domain: 'b24-sp9gpy.bitrix24.ru',
+    //   // client_endpoint: 'https://b24-sp9gpy.bitrix24.ru/rest/',
+    //   // server_endpoint: 'https://oauth.bitrix.info/rest/',
+    //   // member_id: 'f48783b2455adff9acdc00f3e6b2d78f',
+    //   application_token: '8ng5pm4ym2r3hy4rapplypfhx7r99ze9',
+    // },
+    auth: '8ng5pm4ym2r3hy4rapplypfhx7r99ze9',
+  },
+}).then((result) => {
+  console.log(result);
+});
+
+app.post('/', async (req, res) => {
   const { body } = req;
 
-  try {
-    if (!body || !Object.keys(body).length) {
-      return res.sendStatus(400);
-    }
+  console.log(body);
 
-    await axios({
-      method: 'post',
-      url: req.params.url,
-      data: body,
-    });
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
+  res.sendStatus(200);
+
+  // try {
+  //   if (!body || !Object.keys(body).length) {
+  //     return res.sendStatus(400);
+  //   }
+
+  //   await axios({
+  //     method: 'post',
+  //     url: req.params.url,
+  //     data: body,
+  //   });
+  //   res.sendStatus(200);
+  // } catch (error) {
+  //   console.log(error);
+  //   res.sendStatus(500);
+  // }
+});
+
+app.listen(PORT, () => {
+  console.log(`Converter listening on http://localhost:${PORT} !`);
 });
 
 module.exports.endpoint = app;
