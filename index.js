263e917bbf1d6e9ac7b084b5f2815cdbd15f7d17
artginzburg <art.ginzburg@ya.ru>
2021-10-22 20:01:35 +0300

    add some Documentation, add const config to index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 
+const config = {
+  bitrixIncomingWebhook: 'https://b24-sp9gpy.bitrix24.ru/rest/571/j8f8intredn2w3d9/',
+  rickAnalyticsEndpoint: 'https://exchange.rick.ai/transactions/tur-na-kolskiy-ru/',
+};
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -17,7 +22,7 @@ async function crmDealGet(ID) {
   try {
     const apiResponse = await axios({
       method: 'get',
-      url: `https://b24-sp9gpy.bitrix24.ru/rest/571/j8f8intredn2w3d9/crm.deal.get?ID=${ID}`,
+      url: `${config.bitrixIncomingWebhook}crm.deal.get?ID=${ID}`,
     });
 
     return apiResponse?.data?.result;
@@ -63,7 +68,7 @@ app.post('/', async (req, res) => {
   try {
     await axios({
       method: 'post',
-      url: `https://exchange.rick.ai/transactions/tur-na-kolskiy-ru/${rickEvent}`,
+      url: `${config.rickAnalyticsEndpoint}${rickEvent}`,
       data,
     });
   } catch (error) {
@@ -74,7 +79,7 @@ app.post('/', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Converter listening on http://localhost:${PORT} !`);
+  console.log(`API listening on http://localhost:${PORT} !`);
 });
 
 module.exports.endpoint = app;
