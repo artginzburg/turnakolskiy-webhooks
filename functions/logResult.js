210bcb6a73818c29b0136ec0af82048200d97b05
artginzburg <art.ginzburg@ya.ru>
2021-10-25 17:25:52 +0300

    handle !result in logResult

--- functions/logResult.js
+++ functions/logResult.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 
 function logResult(result) {
+  if (!result) {
+    return;
+  }
   console.log(`New log for ID ${result.ID}`);
   fs.appendFileSync(`./logs/results/${result.ID}.json`, JSON.stringify(result));
 }
